1a2eefc5b7bb84a29e9b28eefa14f28ff4b4b6d3
davidseeler <david.seeler@gmail.com>
2020-11-13 20:08:43 -0500

    Animated banner + footer

--- main.php
+++ main.php
@@ -5,6 +5,7 @@
         <link rel="shortcut icon" href="images/favicon.png">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="slider.css">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
@@ -12,13 +13,15 @@
             <nav>
                 <div id="mySidenav" class="sidenav">
                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-                    <a href="#">placeholder</a>
-                    <a href="#">placeholder</a>
-                    <a href="#">placeholder</a>
-                    <a href="#">placeholder</a>
+                    <p>Categories</p>
+                    <a href="#">Fruit</a>
+                    <a href="#">Vegetables</a>
+                    <a href="#">Meat</a>
+                    <a href="#">Seafood</a>
+                    <a href="#">Dairy and Eggs</a>
                 </div>
                 <span id="menuButton" onclick="openNav()">&#9776;</span>
-                <img id="logo" src="images/logo.png">
+                <a href="main.php"><img id="logo" src="images/logo.png"></a>
                 <form id="searchForm">
                     <input id="searchBar" type=text placeholder="Search Products">
                     <button id="searchButton" type=submit><i class="fa fa-search"></i></button>
@@ -27,10 +30,24 @@
                     <a id="accountLink" href="account.html"><image id="accountIcon" src="images/accountIcon.png"></image>Account</a>
                 </div>
                 <div id="cartBox">
-                    <a id="accountLink" href="cart.html"><image id="accountIcon" src="images/shoppingCartIcon.png"></image>Shopping Cart</a>
+                    <a id="cartLink" href="cart.html"><image id="cartIcon" src="images/shoppingCartIcon.png"></image>Shopping Cart</a>
                 </div>
             </nav>
         </header>
+        <main>
+            <div id="slider">
+                <figure>
+                    <img src="images/vegetables.png" alt>
+                    <img src="images/fruit.png" alt>
+                    <img src="images/meat.png" alt>
+                    <img src="images/seafood.png" alt>
+                    <img src="images/dairy.png" alt>
+                </figure>
+            </div>
+        </main>
         <script src="index.js"></script>  
     </body>
+    <footer>
+        <p>&copy; 2020 NED's Grocery</p>
+    </footer>
 </html>
